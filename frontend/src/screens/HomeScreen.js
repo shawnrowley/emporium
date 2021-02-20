9c1f8d318e2f0f96bde15c60ba3f498496015876
shawnrowley <shawn.a.rowley@gmail.com>
2021-02-20 09:29:22 -0500

    initial commit

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Product from "../components/Product";
+import { Row, Col } from "react-bootstrap";
+import axios from "axios";
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products"); // deconstructor res.data
+
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product key={product._id} product={product} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default HomeScreen;
